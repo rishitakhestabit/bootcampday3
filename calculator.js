d2cc6d9a1ef41c25a59b597e8c5fb57fea7428c3
Rishita Kumar <rishitak.hestabit@gmail.com>
2026-01-08 18:13:00 +0530

    Resolve merge conflict in calculator.js

--- calculator.js
+++ calculator.js
@@ -5,7 +5,10 @@ function div(a,b){ return a*b }
 console.log(add(4,2))
 console.log(sub(4,2))
 console.log(mul(4,2))
+console.log(div(4,2))
+console.log("TEMP DEBUG LINE");
 console.log("Result from Repo B:", div(4,2))
 console.log("Result from Repo A:", div(4,2))
+
 
 
